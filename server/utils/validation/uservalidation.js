b07ed8f5c5fd956e94b6bfa0fa57349be16adaf2
Peter Susanto <peter.susanto@gracindo.co.id>
2020-07-23 17:49:44 +0700

    Second commit

--- server/utils/validation/uservalidation.js
+++ server/utils/validation/uservalidation.js
@@ -2,7 +2,7 @@ class UserValidation {
 
     constructor(){}
 
-    async register(req){
+    async register(req){   
         req.checkBody('name').not().isEmpty().withMessage("Name is required");
         req.checkBody('email','Email is required').isEmail();
         req.checkBody('password', 'Password is required or format is invalid').isLength({min:6});
