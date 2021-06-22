45afc94f98f56735a4ec36956f8fadb640055ae3
Peter Susanto <peter.itdev@gmail.com>
2021-06-22 15:45:55 +0700

    update

--- app_background.js
+++ app_background.js
@@ -3,6 +3,9 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const env         = process.env.NODE_ENV || 'localhost';
+const config      = require(__dirname + '/../config/config.json')[env];
+
 const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -17,7 +20,7 @@ app.get('*', (req, res) => res.status(200).send({
 const http = require('http');
 //const app = require('../app'); // The express app we just created
 
-const port = parseInt(process.env.PORT, 10) || 1189;
+const port = parseInt(process.env.PORT, 10) || (config.appPort);
 app.set('port', port);
 
 const server = http.createServer(app);
