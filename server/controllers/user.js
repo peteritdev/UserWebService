5dbaf5fca295dbc556a5570133d2cde9cf86be40
Peter Susanto <peter.susanto@sanqua.co.id>
2022-09-28 15:23:00 +0700

    update

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -284,6 +284,7 @@ async function login(req, res) {
 		req.body.application_id = req.headers['x-application-id'];
 		req.body.device = req.headers['x-device'] == null ? 'web' : req.headers['x-device'];
 		req.body.device_id = req.headers['x-device-id'];
+		console.log(`>>> PARAM : ${JSON.stringify(req.body)}`);
 		joResult = await userServiceInstance.doLogin(req.body);
 	}
 
