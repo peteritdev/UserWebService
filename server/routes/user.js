c9d6f7d2e29dc3d8a47969f6f5876ec3a8e60e2b
Peter Susanto <peter.susanto@sanqua.co.id>
2022-12-06 11:37:28 +0700

    takeout validation email

--- server/routes/user.js
+++ server/routes/user.js
@@ -50,7 +50,7 @@ module.exports = (app) => {
 	];
 	app.get(rootAPIPath + 'user/verify_token', arrValidateVerifyToken, userController.verifyToken);
 
-	var arrValidateForgotPassword = [ check('email').isEmail().withMessage('Invalid email format') ];
+	var arrValidateForgotPassword = [];
 	app.post(rootAPIPath + 'user/forgot_password', arrValidateForgotPassword, userController.forgotPassword);
 
 	var arrValidateVerifyForgotPassword = [ check('code').not().isEmpty().withMessage('Code is required') ];
